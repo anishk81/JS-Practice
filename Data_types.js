b7a936bbceeceb2fa11f6cfdaa688ab4ce5874cc
Anish Kothari <107585475+anishk81@users.noreply.github.com>
2025-09-12 07:07:07 +0000

    Data types in JS

--- Data_types.js
+++ Data_types.js
@@ -1,17 +1,3 @@
-// const username  = "anish1818";
-// let password = "lololo";
-// var userID =  "46401";
-// accountCity =  "Mumbai";
-// let accountState;
-
-// console.log(accountState);
-// console.log(typeof null);
-
-// console.table([username,password,userID, accountCity,accountState]);
-/*
-Never use var. Preferable to use let
-*/
-
 // let score = "33abc";
 // console.log(typeof score); //string
 // let ItsNumber = Number(score)
