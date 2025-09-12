ab7c9802a8c4816dc621c75ba6c5578ceaf83597
Anish Kothari <107585475+anishk81@users.noreply.github.com>
2025-09-12 16:54:30 +0000

    Operations Related Knowledge

---
+++ ConversionOperation.js
@@ -0,0 +1,19 @@
+// console.log(2**3);
+// console.log(2%3);
+// console.log(2*3);
+
+
+// let string3 = "Anish"+"Kothari";
+// console.log(string3)
+
+// console.log("1" + 2 + 2); //122 --> String concatination
+// console.log(1 +2+"2") //32 --> Integer and then string concatination
+
+// console.log(null > 0); //false
+// console.log(null < 0);   //false
+
+// console.log("0" === 0); //false --> Stricter checking including for data types.
+// console.log("0" == 0); // true --> only checks for values
+
+console.log(null >= 0); //true
+console.log(null <= 0);   //true
