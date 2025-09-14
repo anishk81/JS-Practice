3259413c95e7ca869d39b43408d1821a7f342f58
Anish Kothari <107585475+anishk81@users.noreply.github.com>
2025-09-14 08:26:49 +0000

    Data Types in JS

---
+++ DataTypes.js
@@ -0,0 +1,24 @@
+const heroes = ["Spiderman" , "Batman", "Superman"]
+var myObj = {
+    name : "Anish Kothari",
+    age : 22
+}
+
+//console.log(heroes);  -- var and let allows to change later but with const u cant. 
+// console.log(myObj);
+
+/*
+const myFunction = function(){
+    console.log("Anish Kothari");
+}
+myFunction(); // calling the funciton
+console.log(typeof myFunction); //Function
+*/
+/*
+let bigNumber = 3412456893042312n;
+console.log(typeof bigNumber); // bigint
+*/
+
+const sum = Number(22);
+console.log(typeof sum);
+
