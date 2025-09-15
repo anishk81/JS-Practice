6b662fdc38149765bfafdd8dc7414df9db8a18d7
Anish Kothari <107585475+anishk81@users.noreply.github.com>
2025-09-15 09:45:49 +0000

    join objects and some important print formats for objects

---
+++ Objects2.js
@@ -0,0 +1,32 @@
+const user1 = {
+    1 :"A",
+    2 : "B"
+};
+
+const user2 = {
+    3 : "C",
+    4 : "D"
+}
+
+const user3 = {
+    5 : "E",
+    6 : "D"
+}
+
+/*
+const compliedUsers = {...user1, ...user3}; // Spread the object and then combine them.
+
+console.log(compliedUsers);
+*/
+
+const UserDatabase = {
+    id: "ak47",
+    Name: "Anish Kothari",
+    grantedAccess : false
+}
+
+//Very Important
+//console.log(Object.keys(UserDatabase));                    //[ 'id', 'Name', 'grantedAccess' ]
+//console.log(Object.values(UserDatabase));                 //[ 'ak47', 'Anish Kothari', false ]
+//console.log(Object.entries(UserDatabase));                // Gives key and value pairs in nested array
+console.log(UserDatabase.hasOwnProperty("ak47"));           // To check if there is present in that key or no. [No values]
