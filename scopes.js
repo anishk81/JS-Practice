5c256399cf7dc2d571d0db59a98d65a281160ae0
Anish Kothari <107585475+anishk81@users.noreply.github.com>
2025-09-16 10:03:46 +0000

    Scopes in JS

--- scopes.js
+++ scopes.js
@@ -1,3 +1,4 @@
+/*
 const b = 200;
 if(true){
     var a = 10;
@@ -8,3 +9,4 @@ if(true){
 console.log(a); //var is functional scoped
 console.log(b); //const is block scoped 
 console.log(c); //let is also block scoped
+*/
