f7d37a0b7dfdc3647a08c7d35103adc5d2611f07
Anish Kothari <107585475+anishk81@users.noreply.github.com>
2025-09-16 07:58:00 +0000

    Function and block scopes in JS

---
+++ scopes.js
@@ -0,0 +1,10 @@
+const b = 200;
+if(true){
+    var a = 10;
+    const b  = 20;
+    let c = 30;
+}
+
+console.log(a); //var is functional scoped
+console.log(b); //const is block scoped 
+console.log(c); //let is also block scoped
